7f9b75578b8a1a80bce067114baf51115afbc53b
Tibet Tornaci <oofdere@hey.com>
2025-08-23 00:09:05 -0700

    init

---
+++ mod.ts
@@ -0,0 +1,158 @@
+import { Enum, match, pack } from "./enum.ts";
+
+type Range = { min: string; max: string } | string[];
+
+type Rule = {
+    Sequence: (Rule | string)[];
+    Choice: (Rule | string)[];
+    Repeat: {
+        rule: Rule | string;
+        min?: number;
+        max?: number;
+    };
+    Optional: Rule | string;
+    Range: Range;
+    Named: string;
+};
+const Rule = Enum<Rule>();
+type test = Rule["Sequence"];
+
+function stringify(rule: string | Rule): string {
+    if (typeof rule === 'string') {
+        return `"${rule}"`    
+    }
+
+    return match(rule, {
+        Sequence: ($): string =>
+            $.map((e) => typeof e === "string" ? `"${e}"` : stringify(e)).join(
+                " ",
+            ),
+        Choice: ($): string =>
+            `(${$.map((e) => typeof e === "string" ? `"${e}"` : stringify(e)).join(" | ")})`,
+        Repeat: ($) => `(${stringify($.rule)}){${$.min || '0'},${$.max || '7'}}`,
+        Optional: ($) => stringify($) + "?",
+        Range: ($) => {
+            if (typeof $ === 'object') {
+                return `[${$.min}-${$.max}]`
+            } else {
+                return $
+            }
+        },
+        Named: ($) => $,
+    });
+}
+
+// make a function that 
+
+function grammar<T>(
+    rules: {
+        [K in keyof T]:
+            | string
+            | Rule
+            | ((self: { [K in keyof T]: Rule }) => Rule);
+    },
+) {
+    // disgusting hack to intercept strings and make them into rules when stringifying
+    const proxy = Object.fromEntries(Object.entries(rules).map(([name, rule]) => [name, Rule('Named', name)]))
+    console.log(Object.entries(rules))
+    let g = []
+    for (const [name, rule] of Object.entries(rules)) {
+        console.log(
+            name, typeof rule, stringify(typeof rule === "function" ? rule(proxy) : rule)
+        );
+        g.push(`${name} ::= ${stringify(typeof rule === "function" ? rule(proxy) : rule)}`)
+    }
+    return g.join("\n");
+}
+
+// ! figure out some way to allow any kind of whitespace like the extras field in tree-sitter does
+
+
+
+import {getLlama
+, LlamaCompletion
+} from "npm:node-llama-cpp";
+
+const llama= await getLlama()
+const model = await llama.loadModel({
+    modelPath: '/Users/teo/hug/silicon-maid-7b.Q6_K.gguf'
+})
+const context = await model.createContext()
+const completion = new LlamaCompletion({
+    contextSequence: context.getSequence()
+})
+
+const myGrammar = grammar({
+    root: ($) => seq($.hello,  optional(seq("today is ", $.day, optional($.remark), "!"))),
+
+    hello: $ => seq("hello, ", $.word, "! my word is ", $.word, "! "),
+    word: repeat(choice(range("a", "z"), range("A", "Z"))),
+    day: $ => seq(range("A", "Z"), $.word, "day"),
+
+    remark: $ => choice(seq(" and what a ", $.word, " day it is!"), seq(" and I'm super ", $.word, " to ", $.word, " with you today!"))
+});
+console.log("\n", myGrammar, "\n")
+const prompt = "You are Yume, a computer friend! you can call the user oofdere, oof, or oofie, and today is Saturday, August 22nd, 1.525. Yume: "
+
+const compiled = await llama.createGrammar({
+    grammar: myGrammar
+})
+
+console.log("prompt: ", prompt)
+console.log("gen: ", (await completion.generateCompletion(prompt, {
+    temperature: 1.5,
+    grammar: compiled
+})))
+console.log("gen: ", (await completion.generateCompletion(prompt, {
+    temperature: 1.5,
+    grammar: compiled
+})))
+console.log("gen: ", (await completion.generateCompletion(prompt, {
+    temperature: 1.5,
+    grammar: compiled
+})))
+console.log("gen: ", (await completion.generateCompletion(prompt, {
+    temperature: 1.5,
+    grammar: compiled
+})))
+console.log("gen: ", (await completion.generateCompletion(prompt, {
+    temperature: 1.5,
+    grammar: compiled
+})))
+console.log("gen: ", (await completion.generateCompletion(prompt, {
+    temperature: 1.5,
+    grammar: compiled
+})))
+
+
+/** This function creates a rule that matches any number of other rules, one after another. */
+export function seq(...rules: (Rule | string)[]): Rule {
+    return Rule("Sequence", rules);
+}
+
+/** This function creates a rule that matches one of a set of possible rules. The order of the arguments does not matter. */
+export function choice(...rules: (Rule | string)[]): Rule {
+    return Rule("Choice", rules);
+}
+
+/** This function creates a rule that matches *zero-or-more* occurrences of a given rule. */
+export function repeat(rule: string | Rule): Rule {
+    return Rule("Repeat", { rule });
+}
+
+/** This function creates a rule that matches *one-or-more* occurrences of a given rule. The previous repeat rule is implemented in repeat1 but is included because it is very commonly used. */
+export function repeat1(rule: string | Rule): Rule {
+    return Rule("Repeat", { rule, min: 1 });
+}
+
+/** This function creates a rule that matches zero or one occurrence of a given rule. */
+export function optional(rule: string | Rule): Rule {
+    return Rule("Optional", rule);
+}
+
+/**
+ * This rule matches every character in the range provided
+ */
+export function range(from: string, to: string) {
+    return Rule("Range", {min: from, max: to});
+}
---
+++ readme.md
@@ -0,0 +1,48 @@
+# oke [><](https://www.youtube.com/watch?v=tog6_AZO4p4) (wiiiiip)
+
+dynamically generate [gbnf](https://github.com/ggml-org/llama.cpp/blob/b55f06e1aa67fb10e89f53e31bbccf37eb2678ea/grammars/README.md) grammars
+
+the code is a mess so unless you want to initialize llama with a rp model every time and generate 5 completions maybe wait for me to clean it up
+
+## defining rules
+the grammar DSL is largely based on [tree-sitter](https://tree-sitter.github.io/tree-sitter/creating-parsers/2-the-grammar-dsl.html)'s, but with the ability to reference external grammars (partially or fully) as rules as well, so you can easily compose multiple grammars
+
+```ts
+const myGrammar = grammar({
+    root: ($) => seq($.hello,  optional(seq("today is ", $.day, optional($.remark), "!"))),
+
+    hello: $ => seq("hello, ", $.word, "! my word is ", $.word, "! "),
+    word: repeat(choice(range("a", "z"), range("A", "Z"))),
+    day: $ => seq(range("A", "Z"), $.word, "day"),
+
+    remark: $ => choice(seq(" and what a ", $.word, " day it is!"), seq(" and I'm super ", $.word, " to ", $.word, " with you today!"))
+});
+```
+
+compiles to
+
+```ebnf
+root ::= hello "today is " day remark? "!"?
+hello ::= "hello, " word "! my word is " word "! "
+word ::= (([a-z] | [A-Z])){0,7}
+day ::= [A-Z] word "day"
+remark ::= (" and what a " word " day it is!" | " and I'm super " word " to " word " with you today!") 
+```
+
+## roadmap
+this will eventually become a llamacpp wrapper
+
+it is inevitable
+
+also that would make it reasonable to use [llguidance](https://github.com/ggml-org/llama.cpp/blob/master/docs/llguidance.md) which requires a custom build of llamacpp but is way faster and already has libs for making and testing grammars in every language
+
+also possibly binding with the webgpu build that's a wip rn
+
+and more primatives, also schema validation
+
+---
+
+> I looked at tree-sitter and asked
+> > what would happen if i fwell off the tree
+
+[give me money if you find this useful](https://github.com/sponsors/oofdere/)
